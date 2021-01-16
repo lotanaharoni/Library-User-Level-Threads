8bac869b769edfeb38ed82f1b2faecf1dc4d627a
lotanaharoni <50168804+lotanaharoni@users.noreply.github.com>
2021-01-16 10:23:35 +0200

    Update Thread.h

--- Thread.h
+++ Thread.h
@@ -12,70 +12,37 @@
 typedef unsigned long address_t;
 
 
-/**
- * Thread - a Thread class
- */
 class Thread {
 
 private:
 
-	/**
-	 * the thread's id
-	 */
 	int _id;
 
-	/**
-	 *  the thread's state
-	 */
 	int _state;
 
-	/**
-	 *  the thread's priority
-	 */
 	int _priority;
-
-	/**
-	 *  the thread's quantum counter
-	 */
+	
 	int _quantum_counter;
 
-	/**
-	 *  the thread's function
-	 */
 	void (*_f)();
 
-	/**
-	 *  the thread's stack
-	 */
 	char* _stack;
 
-	/**
-	 *  thread termination flag
-	 */
 	bool _terminated;
 
-	/**
-	 *  thread pc pointer.
-	 */
 	address_t _pc;
 
-	/**
-	 *  thread sp pointer.
-	 */
 	address_t _sp;
 
-	/**
- 	 *  thread buff.
- 	 */
 	sigjmp_buf _env;
 
 public:
 
 	/**
 	 * a constructor of a Thread object
-	 * @param id - the thread's id
-	 * @param priority - the thread's priority
-	 * @param f - the thread's function
+	 * @param id - The thread's id
+	 * @param priority - The thread's priority
+	 * @param f - The thread's function
 	 */
 	Thread(int id, int priority, void(*f)());
 
@@ -113,22 +80,22 @@ public:
 	int get_quantum_counter() const;
 
 	/**
-	 * adds 1 to the counter.
+	 * Adds 1 to the counter.
 	 */
 	void increment_counter();
 
 	/**
-	* @return the Thread's termination flag
+	* Return the Thread's termination flag
 	*/
 	bool get_terminated() const;
 
 	/**
-	 * flags this thread as terminated.
+	 * Flags this thread as terminated.
 	 */
 	void set_terminated();
 
 	/**
-	 * returns the envelop with the data for
+	 * Returns the envelop with the data for
 	 * sigjump
 	 */
 	sigjmp_buf& get_env();
